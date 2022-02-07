92f5e3021ce5149c5d614ab6496e6f9021c05dc9
Paulius <arsnumeralis@gmail.com>
2022-02-07 12:46:32 +0000

    basic automation now works

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ import sys
 from urllib import request as urlreq
 import json
 
+#initialising web-app
 app = Flask(__name__)
 
+#specifying the url for the below code and assigning http methods
 @app.route("/", methods = ["GET", "POST"])
 def Homepage():
+    #Request details written as a dictionary so they can be selected depending on the queried city
     reqs = {
         "London":{
             "city_id":"2643743",
@@ -32,6 +35,7 @@ def Homepage():
     api_key = "9d50450a48809637b4862bdcb125927d"
     if request.method == "POST":
         city = request.form["city_select"]
+        #below requests data from the weather api, converts it into a JSON object, relevant parts of which can then be displayed on the html
         data = urlreq.urlopen(f'http://api.openweathermap.org/data/2.5/weather?id={reqs[city]["city_id"]}&units=metric&lang={reqs[city]["language"]}&appid={api_key}').read()
         weather_data = json.loads(data)
         display_data = {
@@ -43,8 +47,10 @@ def Homepage():
             "max_temp": str(weather_data["main"]["temp_max"])+"°C"
         }
     else:
+        #if no post request (opening the page for the 1st time), no data is returned
         display_data = None
     return render_template("index.html", display_data = display_data)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5001", debug=True)
+    #if the app is run directly, changes to the code can be made without killing the session
+    app.run(debug=True)
--- tests/tests.py
+++ tests/tests.py
@@ -1,9 +1,69 @@
+from tkinter.tix import Select
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+import time
+import sys
 
+#data for querying cities
+api_key = "9d50450a48809637b4862bdcb125927d"
+cities = {
+    "london": {
+        "name":"London",
+        "htmlid":"lnd",
+        "apiid":"2643743"
+        },
+    "paris": {
+        "name":"Paris",
+        "htmlid":"par",
+        "apiid":"2988507"
+    },
+    "newyork": {
+        "name":"New York",
+        "htmlid":"nyc",
+        "apiid":"5128581"
+    },
+    "delhi": {
+        "name":"Delhi",
+        "htmlid":"del",
+        "apiid":"2650225"
+    },
+    "tokyo": {
+        "name":"Tokyo",
+        "htmlid":"tok",
+        "apiid":"2988507"
+    }
+}
+
+#installs driver automatically - user needs to ensure they have Google Chrome installed before running
 s=Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=s)
-driver.maximize_window()
-driver.get('https://www.google.com')
+options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+
+#having it as a function prevents the window from closing immediately (likely due to garbage collection)
+def launcher():
+    driver = webdriver.Chrome(service=s, options=options)
+    #when running the script, paste the url of the web-app as a command line argument
+    driver.get(r"http://127.0.0.1:5000/")
+    return driver
+
+#if no error, t01 passed.
+driver = launcher()
+
+def test_case_query(city_id):
+    selection1 = driver.find_element_by_id("city_select")
+    selection1.click()
+    time.sleep(1)
+    selection2 = driver.find_element_by_id(city_id)
+    selection2.click()
+    time.sleep(1)
+    submission = driver.find_element_by_id("sub")
+    return submission.click()
+
+for city in cities:
+    test_case_query(cities[city]["htmlid"])
+
+
+
